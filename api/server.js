ba57a06ad9a627e76836683466a1923031a4286f
Charles Qian <charleshqian@gmail.com>
2021-08-30 15:47:05 -0700

    completed most of commands

--- api/server.js
+++ api/server.js
@@ -1,3 +1,43 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model.js')
+const server = express()
+server.use(express.json())
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req,res) => {
+    User.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message:'The users information could not be retrieved'})
+    })
+})
+
+server.get('/api/users/:id', (req,res) => {
+    User.findById(req.params.id)
+    .then(user => user ? res.status(200).json(user) : res.status(404).json({message: "The user with the specified ID does not exist"}))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message:'The user information could not be retrieved'})
+    })
+})
+
+server.post('/api/users', (req,res) => {
+    User.insert(req.body)
+    .then(newUser => newUser ? res.status(201).json(newUser): res.status(400).json({ message: "Please provide name and bio for the user" }))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message:'There was an error while saving the user to the database'})
+    })
+})
+
+server.delete('/api/users/:id', (req,res) => {
+    User.remove(req.params.id)
+    .then(user => user ? res.status(200).json(user) : res.status(404).json({message: "The user with the specified ID does not exist"}))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message:'The user could not be removed'})
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server.js');
 const port = 5000;
 
 // START YOUR SERVER HERE
-server.listen(3000, () => {
-    console.log(`listening on port ${port}`)
+server.listen(port, () => {
+    console.log('listening on', port)
 })
