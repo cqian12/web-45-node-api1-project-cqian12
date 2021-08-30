7e4cd01b144e5acccd91315ae05fa830da581d23
Charles Qian <charleshqian@gmail.com>
2021-08-30 16:13:23 -0700

    completed MVP

--- api/server.js
+++ api/server.js
@@ -23,8 +23,11 @@ server.get('/api/users/:id', (req,res) => {
 })
 
 server.post('/api/users', (req,res) => {
-    User.insert(req.body)
-    .then(newUser => newUser ? res.status(201).json(newUser): res.status(400).json({ message: "Please provide name and bio for the user" }))
+    const newUser = req.body
+
+    !newUser.name || !newUser.bio ? res.status(400).json({ message: "Please provide name and bio for the user" })
+    : User.insert(req.body)
+    .then(user => res.status(201).json(user))
     .catch(err => {
         console.log(err)
         res.status(500).json({message:'There was an error while saving the user to the database'})
@@ -37,6 +40,18 @@ server.delete('/api/users/:id', (req,res) => {
     .catch(err => {
         console.log(err)
         res.status(500).json({message:'The user could not be removed'})
+    })
+})
+
+server.put('/api/users/:id', (req,res) => {
+    const newInfo = req.body
+
+    !newInfo.name || !newInfo.bio ? res.status(400).json({ message: "Please provide name and bio for the user" })
+    : User.update(req.params.id, newInfo)
+    .then(updatedUser => updatedUser ? res.status(200).json(updatedUser) : res.status(404).json({message: "The user with the specified ID does not exist"}))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message:'The user information could not be modified'})
     })
 })
 
